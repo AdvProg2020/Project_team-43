d4613291941b6b72dc6add252425834ab9348942
alireza <ali.d.f.2012@gmail.com>
2020-06-09 12:09:01 +0430

    background for register

--- src/main/java/Controller/Graphic/RegisterMenuController.java
+++ src/main/java/Controller/Graphic/RegisterMenuController.java
@@ -1,9 +1,12 @@
 package Controller.Graphic;
 
 import Controller.console.BuyerProcessor;
+import View.graphic.MainWindow;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.DeliveryStatus;
 import model.UserPersonalInfo;
 
@@ -15,12 +18,25 @@ public class RegisterMenuController {
     public TextField email;
     public TextField companyName;
     public PasswordField passWord;
+    public Label invalidUserName;
+    public Label invalidPassWord;
     private BuyerProcessor buyerProcessor = BuyerProcessor.getInstance();
 
-    public void register(ActionEvent actionEvent) {
+    public void registerButtonClicked(ActionEvent actionEvent) {
+        invalidPassWord.setVisible(false);
+        invalidUserName.setVisible(false);
+        if (passWord.getText().length() < 8) {
+            invalidPassWord.setVisible(true);
+            return;
+        }
         UserPersonalInfo userPersonalInfo = new UserPersonalInfo(firstName.getText(), lastName.getText(), email.getText()
                 , phoneNumber.getText(), passWord.getText());
-        buyerProcessor.register(userPersonalInfo, userName.getText(), companyName.getText());
+        if (buyerProcessor.register(userPersonalInfo, userName.getText(), companyName.getText()).equals("done")) {
+            ((Stage) userName.getScene().getWindow()).close();
+            MainWindow.getInstance().start(MainWindow.getInstance().getStage());
+        } else {
+            invalidUserName.setVisible(true);
+        }
 
     }
 }
