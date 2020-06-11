9ef0a7af62251bd539b02e900e078c6b535e12c1
alireza <ali.d.f.2012@gmail.com>
2020-06-11 20:52:41 +0430

    ManagerMenuController

--- src/main/java/Controller/Graphic/ManagerMenuController.java
+++ src/main/java/Controller/Graphic/ManagerMenuController.java
@@ -1,4 +1,21 @@
 package Controller.Graphic;
 
-public class ManagerMenuController {
+import Controller.console.BossProcessor;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import model.UserPersonalInfo;
+
+public class ManagerMenuController extends Controller {
+    BossProcessor bossProcessor = BossProcessor.getInstance();
+    public TextField firstName;
+    public TextField lastName;
+    public TextField email;
+    public TextField password;
+    public TextField phoneNumber;
+
+    public void update(ActionEvent actionEvent) {
+        UserPersonalInfo userPersonalInfo = new UserPersonalInfo(firstName.getText(), lastName.getText(), email.getText()
+                , phoneNumber.getText(), password.getText());
+        bossProcessor.editField(userPersonalInfo);
+    }
 }
--- src/main/java/View/graphic/BuyerUserWindow.java
+++ src/main/java/View/graphic/BuyerUserWindow.java
@@ -37,4 +37,5 @@ public class BuyerUserWindow extends Application {
         primaryStage.show();
 
     }
+
 }
