51816654fa6382764f990ac06a530a0135c881f7
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2020-06-12 16:02:47 +0430

    creating id and page

--- src/main/java/Controller/Graphic/ProductPanelController.java
+++ src/main/java/Controller/Graphic/ProductPanelController.java
@@ -4,7 +4,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import model.Category;
 
 import java.net.URL;
@@ -12,38 +14,101 @@ import java.util.ResourceBundle;
 
 public class ProductPanelController implements Initializable {
 
+    @FXML
+    ImageView image1;
+    @FXML
+    ImageView image2;
+    @FXML
+    ImageView image3;
+    @FXML
+    ImageView image4;
+    @FXML
+    ImageView image5;
+    @FXML
+    ImageView image6;
+    @FXML
+    ImageView image7;
+    @FXML
+    ImageView image8;
+    @FXML
+    ImageView image9;
+    @FXML
+    Label name1;
+    @FXML
+    Label name2;
+    @FXML
+    Label name3;
+    @FXML
+    Label name4;
+    @FXML
+    Label name5;
+    @FXML
+    Label name6;
+    @FXML
+    Label name7;
+    @FXML
+    Label name8;
+    @FXML
+    Label name9;
+    @FXML
+    Label price1;
+    @FXML
+    Label price2;
+    @FXML
+    Label price3;
+    @FXML
+    Label price4;
+    @FXML
+    Label price5;
+    @FXML
+    Label price6;
+    @FXML
+    Label price7;
+    @FXML
+    Label price8;
+    @FXML
+    Label price9;
 
-    public void sortByView(){
+
+    public void sortByView() {
 
     }
 
-    public void sortByDate(){
+    public void sortByDate() {
 
     }
 
-    public void sortByScore(){
+    public void sortByScore() {
 
     }
 
-    public void sortByPrice(){
+    public void sortByPrice() {
 
     }
 
-    public void filter(){
+    public void filter() {
 
     }
 
-    public void showCategories(){
+    public void nextPage() {
+
+    }
+
+    public void previousPage() {
+
+    }
+
+    public void showCategories() {
         categoryListView.setVisible(true);
     }
 
-    public void selectedCategory(){
+    public void selectedCategory() {
         int index = categoryListView.getSelectionModel().getSelectedIndex();
         filterByCategory(categories.get(index));
         categoryListView.setVisible(false);
     }
 
-    public void filterByCategory(String categoryName){
+    public void filterByCategory(String categoryName) {
 
     }
 
