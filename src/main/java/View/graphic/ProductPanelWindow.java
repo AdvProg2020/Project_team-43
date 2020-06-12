d6a49a39687463f94a603631f3f5aec3d30bbf47
alireza <ali.d.f.2012@gmail.com>
2020-06-12 15:15:00 +0430

    test for product panel

---
+++ src/main/java/View/graphic/ProductPanelWindow.java
@@ -0,0 +1,39 @@
+package View.graphic;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ProductPanelWindow extends Application {
+    private static ProductPanelWindow instance = new ProductPanelWindow();
+
+    public static ProductPanelWindow getInstance() {
+        return instance;
+    }
+
+    private Stage stage;
+
+    private ProductPanelWindow() {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("productPanel.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Products");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+}
