b4cf8a6aaa68e839fbc3ade71e2a61a0420de69a
alireza <ali.d.f.2012@gmail.com>
2020-06-18 19:21:30 +0430

    price/available/name filter

--- src/main/java/Controller/Graphic/ProductPanelController.java
+++ src/main/java/Controller/Graphic/ProductPanelController.java
@@ -1,24 +1,68 @@
 package Controller.Graphic;
 
+import Controller.console.BuyerProcessor;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import model.Category;
+import model.Product;
+import org.controlsfx.control.RangeSlider;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ProductPanelController extends Controller implements Initializable {
-
+    private final BuyerProcessor buyerProcessor = BuyerProcessor.getInstance();
+    public TextField nameFilterText;
+    public RangeSlider priceFilterSlider;
+    public Label minValue;
+    public Label maxValue;
+    private ArrayList<Product> allProducts = Product.getAllProductsInList();
+    public CheckBox availableFilterCheckBox;
     @FXML
     private ListView<String> categoryListView;
 
     ObservableList<String> categories;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        setMaxAndMinPrice();
+        priceFilterSlider.highValueProperty().addListener((observable) -> {
+            maxValue.setText(Double.toString((int) priceFilterSlider.getHighValue()));
+
+        });
+        priceFilterSlider.lowValueProperty().addListener((observable -> {
+            minValue.setText(Double.toString((int) priceFilterSlider.getLowValue()));
+        }));
+        BuyerProcessor.getInstance().newProductFilter();
+        categoryListView.setVisible(false);
+        for (Category category : Category.getAllCategories()) {
+            categories.add(category.getName());
+        }
+        categoryListView.setItems(categories);
+    }
+
+    public void setMaxAndMinPrice() {
+        double maxPrice = 0;
+        for (Product product : allProducts) {
+            if (product.getPrice() > maxPrice)
+                maxPrice = product.getPrice();
+        }
+        priceFilterSlider.setMax(maxPrice);
+        priceFilterSlider.setHighValue(maxPrice);
+        minValue.setText("0");
+        maxValue.setText(Double.toString(maxPrice));
+
+    }
 
     public void sortByView() {
 
@@ -37,6 +81,13 @@ public class ProductPanelController extends Controller implements Initializable 
     }
 
     public void filter() {
+        if (!nameFilterText.getText().equals("")) {
+            buyerProcessor.filter("by name " + nameFilterText.getText());
+
+        } else {
+            buyerProcessor.disableFilter("name");
+        }
+        buyerProcessor.filter("by price from " + minValue.getText() + " to " + maxValue.getText());
 
     }
 
@@ -67,17 +118,16 @@ public class ProductPanelController extends Controller implements Initializable 
         categories = FXCollections.observableArrayList();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        categoryListView.setVisible(false);
-        for (Category category : Category.getAllCategories()) {
-            categories.add(category.getName());
-        }
-        categoryListView.setItems(categories);
-    }
-
 
     public void hideCategories() {
         categoryListView.setVisible(false);
+    }
+
+    public void filterAvailable() {
+        if (availableFilterCheckBox.isSelected()) {
+            buyerProcessor.filter("by availability");
+        } else {
+            buyerProcessor.disableFilter("availability");
+        }
     }
 }
