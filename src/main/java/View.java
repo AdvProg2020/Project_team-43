d03b239cc92093921241abfb34078d5ba318a184
alireza <ali.d.f.2012@gmail.com>
2020-07-11 18:28:12 +0430

    server register

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -15,44 +15,15 @@ import View.Client;
 public class View extends Application {
     @Override
     public void start(Stage primaryStage) {
-            Client client = new Client();
-            client.run();
-            Controller.setClient(client);
-            MainWindow.getInstance().start(primaryStage);
+        Client client = new Client();
+        client.run();
+        Controller.setClient(client);
+        MainWindow.getInstance().start(primaryStage);
     }
 
     public static void main(String[] args) {
         //App.getInstance().open();
         launch(args);
-
     }
 
-    public void createFirstManager() {
-        TextInputDialog textInputDialog = new TextInputDialog();
-        textInputDialog.setHeaderText("first manager");
-        Label label1 = new Label("username: ");
-        Label label2 = new Label("password: ");
-        TextField text1 = new TextField();
-        TextField text2 = new TextField();
-        GridPane grid = new GridPane();
-        grid.add(label1, 1, 1);
-        grid.add(text1, 2, 1);
-        grid.add(label2, 1, 2);
-        grid.add(text2, 2, 2);
-        textInputDialog.getDialogPane().setContent(grid);
-        textInputDialog.setTitle("register");
-        textInputDialog.getDialogPane().lookupButton(ButtonType.CANCEL).setVisible(false);
-        final Button btOk = (Button) textInputDialog.getDialogPane().lookupButton(ButtonType.OK);
-        btOk.addEventFilter(
-                ActionEvent.ACTION,
-                event -> {
-                    if (text1.getText().equals("") || text2.getText().equals("")) {
-                        event.consume();
-                    } else {
-                        new Manager(text1.getText(), new UserPersonalInfo("", "", "", "", text2.getText()));
-                    }
-                }
-        );
-        textInputDialog.showAndWait();
-    }
 }
